6a3727de666627e3026f80b895bb76a1afbf5782
WebSepp <bastilange@t-online.de>
2020-04-17 18:46:13 +0200

    changed design of datamodelling maybe rallback at this point

--- assets/js/vue_module.js
+++ assets/js/vue_module.js
@@ -9,113 +9,138 @@ var main = new Vue ({
     data: {
         image: 'images/oaguidelines_modell.jpeg',
         datamodelldefaultImage: 'images/DataModells/AGATE_Modell_Alt_Web.png',
-        datamodelldefaultTitle: "Altes Datenmodell",
-        datamodelldefaultText: "Das alte AGATE Datenmodell umfasste lediglich die Entitäten Projekt, Person und Organisationseinheit.",
+        datamodelldefaultTitle: "Altes AGATE Datenmodell",
+        datamodelldefaultText: "Das alte AGATE Datenmodell umfasste lediglich die Entitäten Projekt, Person und Organisationseinheit. Diese sind bereits mit einem global einzigartigem persistenten Identifier versehen und könnten so bereits durch Relationen untereinander Rollen in der Anwendungsdomäne spezifizieren. Beispielsweise die Rolle die eine Person innerhalb eines Projektes oder einer Organisationseinheit innehat. Es besteht keine Verbindung, die es ermöglichen würde Daten aus externen Systemen einzuspeisen und somit den Datensatz anzureichern.",
+        agateentitydefaultTitle: "Neues AGATE Datenmodell mit allen OpenAIRE Elementen",
+        agateentitydefaultText: "Die Abbildung zeigt das neue Datenmodell mit den untereinander verlinkten Entitäten und den Elementen, die sie abbilden können.",
+        agateentitydefaultImage: 'images/DataModells/All_OpenAIRE_Elements_Web.png',
+        agateentitydefaultType: "Alle OpenAIRE Elemente",
+        overviewElementdefaultId: 2902,
+        overviewElementdefaultTitle: "Alle in AGATE erfassten Daten",
+        overviewElementdefaultlText: "Die Grafik zeigt noch einmal alle in AGATE erfassten Daten, die sich nach dem alten Datenmodell richten und lediglich Verbindung zu der Projekt Entität innehaben.",
+        overviewElementdefaultImage: 'images/DataModells/AGATE_Daten_Alt_Web.png',
+        overviewElementdefaultType: "Alle AGATE Daten",
         updated: false,
+
         datamodells:[
             {
                 datamodellId: 2901,
                 datamodellTitle: "Altes AGATE Datenmodell",
-                datamodellText: "Das alte AGATE Datenmodell umfasste lediglich die Entitäten Projekt, Person und Organisationseinheit.",
+                datamodellText: "Das alte AGATE Datenmodell umfasste lediglich die Entitäten Projekt, Person und Organisationseinheit. Diese sind bereits mit einem global einzigartigem persistenten Identifier versehen und könnten so bereits durch Relationen untereinander Rollen in der Anwendungsdomäne spezifizieren. Diese Art von Relation besteht aufgrund der Datenlage allerdings bisher immer nur ausgehend vom Projekt. Beispielsweise die Rolle die eine Person innerhalb eines Projektes oder einer Organisationseinheit innehat. Es besteht keine Verbindung, die es ermöglichen würde Daten aus externen Systemen einzuspeisen und somit den Datensatz anzureichern.",
                 datamodellImage: 'images/DataModells/AGATE_Modell_Alt_Web.png',
                 datamodellType: "Altes Modell"
 
             },
             {
-                datamodellId: 2902,
-                datamodellTitle: "Alle in AGATE erfassten Daten",
-                datamodellText: "Die Grafik zeigt noch einmal alle in AGATE erfassten Daten, die sich nach dem alten Datenmodell richten und lediglich Verbindung zu der Projekt Entität innehaben.",
-                datamodellImage: 'images/DataModells/AGATE_Daten_Alt_Web.png',
-                datamodellType: "Alle AGATE Daten"
-
-            },
-            {
                 datamodellId: 2903,
                 datamodellTitle: "Neues AGATE Datenmodell",
-                datamodellText: "In dem neuen AGATE Datenmodell kommen die Entitäten Funding, Publication und Product zu den bereits erfassten Entitäten hinzu und sind zusätzlich noch untereinander und nicht mehr lediglich durch das Projekt verlinkt. Außerdem sollen ORCID und Zenodo als externe Dienstleister zur Anreicherung der Entitäten eingebunden werden.",
+                datamodellText: "In dem neuen AGATE Datenmodell kommen die Entitäten Funding, Publication und Product zu den bereits erfassten Entitäten hinzu. Den FAIR Prinzipien folgend müssen auch diesen Entitäten persistente Identifier zugewiesen werden, sodass sie modular untereinander verlinkbar sind. Das neue Datenmodell soll ebenfalls neue Rollen definieren können. Beispielsweise könnte eine Person nicht mehr nur länger eine Verlinkung zum Projekt innehaben, sondern beispielsweise auch als Autor zu einer Publikation. Durch die Erweiterung des Datenmodells können zahlreiche zusätzliche Forschungsinformationen erfasst werden. Daraus resultiert allerdings ebenfalls ein erheblicher Mehraufwand in der Datenerfassung. Um diesbezüglich Abhilfe zu schaffen kann durch die Einbindung  externer Dienstleister, wie Zenodo (für Publikationen und Produkte) und ORCID (für Personen) dieser Teil der Datenerfassung ausgelagert werden.",
                 datamodellImage: 'images/DataModells/AGATE_Modell_Neu_Web.png',
                 datamodellType: "Neues Modell"
             },
-            {
-                datamodellId: 2904,
-                datamodellTitle: "Neues AGATE Datenmodell mit allen OpenAIRE Elementen",
-                datamodellText: "Die Abbildung zeigt das neue Datenmodell mit den untereinander verlinkten Entitäten und den Elementen, die sie abbilden können.",
-                datamodellImage: 'images/DataModells/All_OpenAIRE_Elements_Web.png',
-                datamodellType: "Alle OpenAIRE Elemente"
-            },
-            {
-                datamodellId: 2905,
-                datamodellTitle: "OpenAIRE Elemente der OrgUnit Entität",
-                datamodellText: "Die folgende Darstellung zeigt die Elemente die nach den OpenAIRE Guidelines unter der OrgUnit Entität abgebildet werden können.",
-                datamodellImage: 'images/DataModells/OrgUnit_Modell_Web.png',
-                datamodellType: "OrgUnit Entität"
-            },
-            {
-                datamodellId: 2906,
-                datamodellTitle: "Mapping der OrgUnit Entität",
-                datamodellText: "Die folgende Darstellung zeigt eine mögliche Zuordnung der bisher in AGATE eingetragenen Daten auf die Elemente der OrgUnit Entität nach den OpenAire Guidelines.",
-                datamodellImage: 'images/DataModells/OrgUnit_Modell_Mapping_Web.png',
-                datamodellType: "OrgUnit Mapping"
-            },
-            {
-                datamodellId: 2907,
-                datamodellTitle: "OpenAIRE Elemente der Person Entität",
-                datamodellText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Person Entität abgebildet werden können.",
-                datamodellImage: 'images/DataModells/Person_Modell_Web.png',
-                datamodellType: "Person Entität"
-            },
-            {
-                datamodellId: 2908,
-                datamodellTitle: "Mapping der Person Entität",
-                datamodellText: "Die folgende Darstellung zeigt eine mögliche Zuordnung der bisher in AGATE eingetragenen Daten auf die Elemente der Person Entität nach den OpenAire Guidelines.",
-                datamodellImage: 'images/DataModells/Person_Modell_Mapping_Web.png',
-                datamodellType: "Person Mapping"
-            },
-            {
-                datamodellId: 2909,
-                datamodellTitle: "OpenAIRE Elemente der Project Entität",
-                datamodellText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Project Entität abgebildet werden können.",
-                datamodellImage: 'images/DataModells/Project_Modell_Web.png',
-                datamodellType: "Project Entität"
-            },
-            {
-                datamodellId: 2910,
-                datamodellTitle: "Mapping der Project Entität",
-                datamodellText: "Die folgende Darstellung zeigt eine mögliche Zuordnung der bisher in AGATE eingetragenen Daten auf die Elemente der Project Entität nach den OpenAire Guidelines.",
-                datamodellImage: 'images/DataModells/Project_Modell_Mapping_Web.png',
-                datamodellType: "Project Mapping"
-            },
-            {
-                datamodellId: 2911,
-                datamodellTitle: "OpenAIRE Elemente der Product Entität",
-                datamodellText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Product Entität abgebildet werden können.",
-                datamodellImage: 'images/DataModells/Product_Modell_Web.png',
-                datamodellType: "Product Entität"
-            },
-            {
-                datamodellId: 2912,
-                datamodellTitle: "OpenAIRE Elemente der Publication Entität",
-                datamodellText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Publication Entität abgebildet werden können.",
-                datamodellImage: 'images/DataModells/Publication_Modell_Web.png',
-                datamodellType: "Publication Entität"
-            },
-            {
-                datamodellId: 2913,
-                datamodellTitle: "OpenAIRE Elemente der Funding Entität",
-                datamodellText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Funding Entität abgebildet werden können.",
-                datamodellImage: 'images/DataModells/Funding_Modell_Web.png',
-                datamodellType: "Funding Entität"
-            },
-            {
-                datamodellId: 2914,
-                datamodellTitle: "Mapping aller Entitäten",
-                datamodellText: "Die folgende Darstellung zeigt eine mögliche Zuordnung aller bisher in AGATE eingetragenen Daten auf alle Elemente und Entitäten nach den OpenAire Guidelines.",
-                datamodellImage: 'images/DataModells/All_Data_All_Mapping_Web.png',
-                datamodellType: "Mapping aller Entitäten"
-            },
-
 
         ],
+
+        agateentities:[
+            {
+                agateentityId: 2904,
+                agateentityTitle: "Neues AGATE Datenmodell mit allen OpenAIRE Elementen",
+                agateentityText: "Die Abbildung zeigt das neue Datenmodell mit den untereinander verlinkten Entitäten und den Elementen, die sie abbilden können.",
+                agateentityImage: 'images/DataModells/All_OpenAIRE_Elements_Web.png',
+                agateentityType: "Alle OpenAIRE Elemente"
+            },
+            {
+                agateentityId: 2905,
+                agateentityTitle: "OpenAIRE Elemente der OrgUnit Entität",
+                agateentityText: "Die folgende Darstellung zeigt die Elemente die nach den OpenAIRE Guidelines unter der OrgUnit Entität abgebildet werden können.",
+                agateentityImage: 'images/DataModells/OrgUnit_Modell_Web.png',
+                agateentityType: "OrgUnit Entität"
+            },
+            {
+                agateentityId: 2906,
+                agateentityTitle: "Mapping der OrgUnit Entität",
+                agateentityText: "Die folgende Darstellung zeigt eine mögliche Zuordnung der bisher in AGATE eingetragenen Daten auf die Elemente der OrgUnit Entität nach den OpenAire Guidelines.",
+                agateentityImage: 'images/DataModells/OrgUnit_Modell_Mapping_Web.png',
+                agateentityType: "OrgUnit Mapping"
+            },
+            {
+                agateentityId: 2907,
+                agateentityTitle: "OpenAIRE Elemente der Person Entität",
+                agateentityText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Person Entität abgebildet werden können.",
+                agateentityImage: 'images/DataModells/Person_Modell_Web.png',
+                agateentityType: "Person Entität"
+            },
+            {
+                agateentityId: 2908,
+                agateentityTitle: "Mapping der Person Entität",
+                agateentityText: "Die folgende Darstellung zeigt eine mögliche Zuordnung der bisher in AGATE eingetragenen Daten auf die Elemente der Person Entität nach den OpenAire Guidelines.",
+                agateentityImage: 'images/DataModells/Person_Modell_Mapping_Web.png',
+                agateentityType: "Person Mapping"
+            },
+            {
+                agateentityId: 2909,
+                agateentityTitle: "OpenAIRE Elemente der Project Entität",
+                agateentityText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Project Entität abgebildet werden können.",
+                agateentityImage: 'images/DataModells/Project_Modell_Web.png',
+                agateentityType: "Project Entität"
+            },
+            {
+                agateentityId: 2910,
+                agateentityTitle: "Mapping der Project Entität",
+                agateentityText: "Die folgende Darstellung zeigt eine mögliche Zuordnung der bisher in AGATE eingetragenen Daten auf die Elemente der Project Entität nach den OpenAire Guidelines.",
+                agateentityImage: 'images/DataModells/Project_Modell_Mapping_Web.png',
+                agateentityType: "Project Mapping"
+            },
+            {
+                agateentityId: 2911,
+                agateentityTitle: "OpenAIRE Elemente der Product Entität",
+                agateentityText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Product Entität abgebildet werden können. Da diese in AGATE bisher nicht erfasst wurden kann auch keine Zuordnung stattfinden.",
+                agateentityImage: 'images/DataModells/Product_Modell_Web.png',
+                agateentityType: "Product Entität"
+            },
+            {
+                agateentityId: 2912,
+                agateentityTitle: "OpenAIRE Elemente der Publication Entität",
+                agateentityText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Publication Entität abgebildet werden können. Da diese in AGATE bisher nicht erfasst wurden kann auch keine Zuordnung stattfinden.",
+                agateentityImage: 'images/DataModells/Publication_Modell_Web.png',
+                agateentityType: "Publication Entität"
+            },
+            {
+                agateentityId: 2913,
+                agateentityTitle: "OpenAIRE Elemente der Funding Entität",
+                agateentityText: "Die folgende Darstellung zeigt  die Elemente die nach den OpenAIRE Guidelines unter der Funding Entität abgebildet werden können. Da diese in AGATE bisher nicht erfasst wurden kann auch keine Zuordnung stattfinden.",
+                agateentityImage: 'images/DataModells/Funding_Modell_Web.png',
+                agateentityType: "Funding Entität"
+            },
+
+        ],
+
+        overviewElements:[
+            {
+                overviewElementId: 2902,
+                overviewElementTitle: "Alle in AGATE erfassten Daten",
+                overviewElementlText: "Die Grafik zeigt noch einmal alle in AGATE erfassten Daten, die sich nach dem alten Datenmodell richten und lediglich Verbindung zu der Projekt Entität innehaben.",
+                overviewElementImage: 'images/DataModells/AGATE_Daten_Alt_Web.png',
+                overviewElementType: "Alle AGATE Daten"
+
+            },
+
+            {
+                overviewElementId: 2904,
+                overviewElementTitle: "Neues AGATE Datenmodell mit allen OpenAIRE Elementen",
+                overviewElementText: "Die Abbildung zeigt das neue Datenmodell mit den untereinander verlinkten Entitäten und den Elementen, die sie abbilden können.",
+                overviewElementImage: 'images/DataModells/All_OpenAIRE_Elements_Web.png',
+                overviewElementType: "Alle OpenAIRE Elemente"
+            },
+            {
+                overviewElementId: 2914,
+                overviewElementTitle: "Mapping aller Entitäten",
+                overviewElementText: "Die folgende Darstellung zeigt eine mögliche Zuordnung aller bisher in AGATE eingetragenen Daten auf alle Elemente und Entitäten nach den OpenAire Guidelines.",
+                overviewElementImage: 'images/DataModells/All_Data_All_Mapping_Web.png',
+                overviewElementType: "Mapping aller Entitäten"
+            },
+        ],
+
         oAentities:[
             {
                 oAentityName:"Person",
@@ -161,6 +186,18 @@ var main = new Vue ({
             this.datamodelldefaultImage = datamodellImage,
                 this.datamodelldefaultTitle = datamodellTitle,
                 this.datamodelldefaultText = datamodellText
+
+        },
+        updateAgateEntity: function(agateentityImage, agateentityText, agateentityTitle){
+            this.agateentitydefaultImage = agateentityImage,
+                this.agateentitydefaultTitle = agateentityTitle,
+                this.agateentitydefaultText = agateentityText
+
+        },
+        updateOverviewElement: function(overviewElementImage, overviewElementText, overviewElementTitle){
+            this.overviewElementdefaultImage = overviewElementImage,
+                this.overviewElementdefaultTitle = overviewElementTitle,
+                this.overviewElementdefaultText = overviewElementText
 
         }
 
